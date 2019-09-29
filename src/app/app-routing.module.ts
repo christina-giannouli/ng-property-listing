66f7fb4b4c52c8b02504692a0bb9d69e393dc64e
christina <xristitina@gmail.com>
2019-09-29 20:33:36 +0300

    creates an initial unit component and updates the app routes array

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { UnitsComponent } from "./units/components/units/units.component";
+import { UnitComponent } from "./units/components/unit/unit.component";
 
-const routes: Routes = [{ path: "units", component: UnitsComponent }];
+const routes: Routes = [
+    { path: "units", component: UnitsComponent },
+    { path: "unit/:id", component: UnitComponent }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { UnitsComponent } from "./units/components/units/units.component";
+import { UnitComponent } from "./units/components/unit/unit.component";
 
 @NgModule({
-    declarations: [AppComponent, UnitsComponent],
+    declarations: [AppComponent, UnitsComponent, UnitComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent]
