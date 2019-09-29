1ec8a5bf8577b456c3d55c3908bd7dbfa0eb567c
christina <xristitina@gmail.com>
2019-09-29 21:38:14 +0300

    creates a login-form initial component, updates app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,34 @@
+/* Modules */
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { AppRoutingModule } from "./app-routing.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+/* Components */
 import { AppComponent } from "./app.component";
 import { UnitsComponent } from "./units/components/units/units.component";
 import { UnitComponent } from "./units/components/unit/unit.component";
 import { LoginComponent } from "./login/components/login/login.component";
+import { LoginFormComponent } from "./login/components/login-form/login-form.component";
+
+/* Services */
+
+/* Guards */
 
 @NgModule({
-    declarations: [AppComponent, LoginComponent, UnitsComponent, UnitComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        UnitsComponent,
+        UnitComponent,
+        LoginFormComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        FormsModule
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
