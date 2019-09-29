9c2ca556986cc40f824f3642dcf3239e1806342b
christina <xristitina@gmail.com>
2019-09-29 21:35:53 +0300

    creates a unit interface

---
+++ src/app/units/interfaces/unit.interface.ts
@@ -0,0 +1,10 @@
+export interface Unit {
+    title: string;
+    region: number;
+    price: number;
+    description: string;
+    cancellation: string;
+    rating: number;
+    availability: number[];
+    amenities: string[];
+}
