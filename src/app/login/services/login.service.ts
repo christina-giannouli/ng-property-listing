9b8e0022f604ca9b538e21338e84cf330ceca72b
christina <xristitina@gmail.com>
2019-09-30 21:40:46 +0300

    updates the login service to make the POST call to the api

--- src/app/login/services/login.service.ts
+++ src/app/login/services/login.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Jwt } from '../../core/interfaces/jwt.interface';
+import { environment } from '../../../environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class LoginService {
+    constructor(private http: HttpClient) {}
 
-  constructor() { }
+    /** it posts the login user to the server and returns a jwt accessToken */
+    login(user: any): Observable<Jwt> {
+        return this.http.post<any>(`${environment.serverUrl}/auth/login`, user).pipe(
+            map((response: any) => response.data),
+            map((jwtEnvelope: any) => {
+                console.log('jwt:', jwtEnvelope);
+
+                return jwtEnvelope;
+            }),
+        );
+    }
 }
