4f8a1f9110ebae3ddda52348ad3a3ee9ad4358ce
christina <xristitina@gmail.com>
2019-10-02 11:24:53 +0300

    adds missing types to login service

--- src/app/login/services/login.service.ts
+++ src/app/login/services/login.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Jwt } from '../../core/interfaces/jwt.interface';
 import { environment } from '../../../environments/environment';
 import { map } from 'rxjs/operators';
+import { LoginUser } from '../interfaces/login-user.interface';
+import { Authentication } from '../../core/interfaces/authentication.interface';
 
 @Injectable({
     providedIn: 'root',
@@ -12,13 +13,10 @@ export class LoginService {
     constructor(private http: HttpClient) {}
 
     /** it posts the login user to the server and returns a jwt accessToken */
-    login(user: any): Observable<Jwt> {
+    login(user: LoginUser): Observable<Authentication> {
         return this.http.post<any>(`${environment.serverUrl}/auth/login`, user).pipe(
-            map((response: any) => response.data),
-            map((jwtEnvelope: any) => {
-                console.log('jwt:', jwtEnvelope);
-
-                return jwtEnvelope;
+            map((authentication: Authentication) => {
+                return authentication;
             }),
         );
     }
