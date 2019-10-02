8c49c597f89999bfb0f61344d629e910095f2c00
christina <xristitina@gmail.com>
2019-10-02 16:32:58 +0300

    updates units component to call the service and send the units to child component

--- src/app/units/components/units/units.component.ts
+++ src/app/units/components/units/units.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { UnitsService } from '../../services/units.service';
+import { Unit } from '../../interfaces/unit.interface';
+import { UnitData } from '../../interfaces/unit-data.interface';
 
 @Component({
-  selector: 'app-units',
-  templateUrl: './units.component.html',
-  styleUrls: ['./units.component.scss']
+    selector: 'app-units',
+    templateUrl: './units.component.html',
+    styleUrls: ['./units.component.scss'],
 })
 export class UnitsComponent implements OnInit {
+    units: UnitData[];
+    totalCount: number;
 
-  constructor() { }
+    constructor(private unitsService: UnitsService) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.getUnits();
+    }
 
+    getUnits(): void {
+        this.unitsService.getUnitList().subscribe(
+            (unit: Unit) => {
+                this.units = unit.data;
+                this.totalCount = unit.meta.totalCount;
+            },
+            (error: any) => {
+                console.error(error);
+            },
+        );
+    }
 }
