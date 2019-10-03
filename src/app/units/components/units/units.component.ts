7bfcae73345b21301d33cd78a74282415fb9ac08
christina <xristitina@gmail.com>
2019-10-03 21:57:02 +0300

    adds app logo and displays user info on top bar, moves app logo styles to global

--- src/app/units/components/units/units.component.ts
+++ src/app/units/components/units/units.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UnitsService } from '../../services/units.service';
 import { Unit } from '../../interfaces/unit.interface';
 import { UnitData } from '../../interfaces/unit-data.interface';
+import { LocalStorageService } from '../../../core/services/localstorage.service';
+import { User } from '../../../core/interfaces/user.interface';
 
 @Component({
     selector: 'app-units',
@@ -11,13 +13,19 @@ import { UnitData } from '../../interfaces/unit-data.interface';
 export class UnitsComponent implements OnInit {
     units: UnitData[];
     totalCount: number;
+    user: User;
 
-    constructor(private unitsService: UnitsService) {}
+    constructor(
+        private unitsService: UnitsService,
+        private localStorageService: LocalStorageService,
+    ) {}
 
     ngOnInit() {
         this.getUnits();
+        this.getUser();
     }
 
+    /* get the list of units from the server */
     getUnits(): void {
         this.unitsService.getUnitList().subscribe(
             (unit: Unit) => {
@@ -28,5 +36,11 @@ export class UnitsComponent implements OnInit {
                 console.error(error);
             },
         );
+    }
+
+    /* gets the user stored on localStorage and parses it to a User object */
+    getUser(): void {
+        const storedUser: string = this.localStorageService.getValue('user');
+        this.user = JSON.parse(storedUser);
     }
 }
