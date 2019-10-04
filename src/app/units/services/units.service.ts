373b3efc4357cf6d0099d5a3b1349ddda075bbec
christina <xristitina@gmail.com>
2019-10-04 22:08:38 +0300

    creates a http post call to units/book

--- src/app/units/services/units.service.ts
+++ src/app/units/services/units.service.ts
@@ -5,6 +5,7 @@ import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Unit } from '../interfaces/unit.interface';
 import { UnitDetails } from '../interfaces/unit-details.interface';
+import { BookedUnit } from '../interfaces/booked-unit.interface';
 
 @Injectable({
     providedIn: 'root',
@@ -23,6 +24,14 @@ export class UnitsService {
     getUnitDetails(id: string): Observable<UnitDetails> {
         return this.http.get<any>(`${environment.serverUrl}/units/${id}`).pipe(
             map((response: UnitDetails) => {
+                return response;
+            }),
+        );
+    }
+
+    bookUnit(unit: BookedUnit): Observable<BookedUnit> {
+        return this.http.post<any>(`${environment.serverUrl}/units/book`, unit).pipe(
+            map((response: BookedUnit) => {
                 return response;
             }),
         );
