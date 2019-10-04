7b9cebebdab888a787553367ea20d634a35a6735
christina <xristitina@gmail.com>
2019-10-04 21:49:59 +0300

    adds unit-details template

--- src/app/units/components/unit-details/unit-details.component.ts
+++ src/app/units/components/unit-details/unit-details.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UnitDetails } from '../../interfaces/unit-details.interface';
 
 @Component({
-  selector: 'app-unit-details',
-  templateUrl: './unit-details.component.html',
-  styleUrls: ['./unit-details.component.scss']
+    selector: 'app-unit-details',
+    templateUrl: './unit-details.component.html',
+    styleUrls: ['./unit-details.component.scss'],
 })
-export class UnitDetailsComponent implements OnInit {
+export class UnitDetailsComponent {
+    @Input() unitDetails: UnitDetails;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    constructor() {}
 }
