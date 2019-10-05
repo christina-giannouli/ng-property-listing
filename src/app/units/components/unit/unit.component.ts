0156e72417d5455c25d461d2b7dc5fdfd1a42d3e
christina <xristitina@gmail.com>
2019-10-05 19:34:17 +0300

    displays rating as stars

--- src/app/units/components/unit/unit.component.ts
+++ src/app/units/components/unit/unit.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { UnitData } from '../../interfaces/unit-data.interface';
 import { BookedUnit } from '../../interfaces/booked-unit.interface';
 
@@ -7,14 +7,26 @@ import { BookedUnit } from '../../interfaces/booked-unit.interface';
     templateUrl: './unit.component.html',
     styleUrls: ['./unit.component.scss'],
 })
-export class UnitComponent {
+export class UnitComponent implements OnInit {
     @Input() unit: UnitData;
     @Input() bookedUnit: BookedUnit;
-    
+
+    starsRating: boolean[] = [];
+
     constructor() {}
+
+    ngOnInit(): void {
+        this.fillRatingStars();
+    }
 
     /* returns the image from the array with the specified size */
     getPictureBySize(size: string): string {
         return this.unit.pictures.find((img: string) => img.includes(size));
+    }
+
+    fillRatingStars(): void {
+        for (let i = 0; i < 5; i++) {
+            this.starsRating.push(i < this.unit.rating);
+        }
     }
 }
